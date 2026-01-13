ba13555ffba32667fb0e830b36e23118ff043a72
Shifan Abdulla <shifan9914@gmail.com>
2026-01-13 14:04:54 +0530

    url updated

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
+def home(request):
+    return JsonResponse({"status": "API running"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/referesh/',TokenRefreshView.as_view(), name='token_refresh'),
     path('api/products/',include("products.urls")),
