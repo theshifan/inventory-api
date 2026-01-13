09fe0750e4ca8c56e36ad0041afde11043dd8528
Shifan Abdulla <shifan9914@gmail.com>
2026-01-13 13:42:11 +0530

    Fix Django deployment config for Render

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-09*%_=#^b!zqmk=axwd0mkij7$zpjk@ikj*i@3kd21f#x)xikt'
+# SECRET_KEY = look into your obsidian app 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
